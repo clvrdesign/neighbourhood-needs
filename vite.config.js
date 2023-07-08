f8881578a47f3bd28e304c1b0e7cffa24344b907
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2023-07-07 22:39:20 -0400

    feat: add config files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const namesOfFolders = ['src','components','img', 'utils']
+const namesOfPaths = namesOfFolders.reduce((accumulator, current) => ({
+  ...accumulator, [current]: `/${current === "src" ? current : "src/" + current}`
+}));
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      ...namesOfPaths
+    },
+  },
 })
+
+
