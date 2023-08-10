955c6c11f35973e2fbd03de7916e7659c35704e2
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2023-08-10 04:36:20 -0400

    feat: add sign in page

--- src/routes/SignIn.jsx
+++ src/routes/SignIn.jsx
@@ -1,6 +1,55 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 function SignIn() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function onChange(event) {
+    setFormData((prevestate) => ({
+      ...prevestate,
+      [event.target.id]: event.target.value,
+    }));
+  }
+
+  const { email, password } = formData;
   return (
-    <></>
+    <div className="form-container">
+      <header>
+        <h1> Sign In</h1>
+      </header>
+      <form className="registration-form">
+        <input
+          className="registration-form__input"
+          type="email"
+          title="email"
+          id="email"
+          value={email}
+          onChange={onChange}
+          required
+          placeholder="enter email"
+        />
+        <input
+          className="registration-form__input"
+          type="password"
+          title="password"
+          id="password"
+          value={password}
+          onChange={onChange}
+          required
+          minLength={8}
+          maxLength={32}
+          placeholder="enter password"
+        />
+
+        <button className="registration-form__signUpBtn">Sign In</button>
+      </form>
+      <Link className="registration-link" to="/sign-up">
+        Sign up Instead
+      </Link>
+    </div>
   );
 }
 
--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,35 +1,64 @@
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+
 function SignUp() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  function onChange(event) {
+    setFormData((prevestate) =>({
+      ...prevestate,
+      [event.target.id]:event.target.value
+    }) )
+  }
+
+  const {name, email, password} = formData
   return (
     <div className="form-container">
       <header>
-        <h1> Sign Up Here</h1>
+        <h1> Sign Up </h1>
       </header>
       <form className="registration-form">
         <input
           className="registration-form__input"
           type="text"
           title="name"
+          id="name"
+          value={name}
+          onChange={onChange}
+          required
           placeholder="enter first and last name"
         />
         <input
           className="registration-form__input"
           type="email"
           title="email"
+          id="email"
+          value={email}
+          onChange={onChange}
+          required
           placeholder="enter email"
         />
         <input
           className="registration-form__input"
           type="password"
           title="password"
+          id="password"
+          value={password}
+          onChange={onChange}
+          required
+          minLength={8}
+          maxLength={32}
           placeholder="enter password"
         />
 
         <button className="registration-form__signUpBtn">Sign Up</button>
       </form>
       <Link className="registration-link" to="/sign-in">
-        {" "}
-        Sign in Instead{" "}
+        Sign in Instead
       </Link>
     </div>
   );
