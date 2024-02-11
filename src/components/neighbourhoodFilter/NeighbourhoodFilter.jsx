4951e3729e3a2eb2dd463770d2ccf011689cd363
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2024-02-11 14:49:15 -0400

    refactor(NeightbourhoodFilter.jsx): control appearance with state

--- src/components/neighbourhoodFilter/NeighbourhoodFilter.jsx
+++ src/components/neighbourhoodFilter/NeighbourhoodFilter.jsx
@@ -1,37 +1,33 @@
-import { sortedNeighbourhoods } from "utils/extractNeighbourhoods.js";
+import {sortedNeighbourhoods} from "utils/extractNeighbourhoods.js";
 import { useEffect, useState } from "react";
 
-  let showFilter = false;
-  if (typeof window !== "undefined") {
-    document.addEventListener("DOMContentLoaded", function () {
-      const mediaQuery = window.matchMedia("(min-width: 1200px)");
 
-      if (mediaQuery.matches) {
-        showFilter = true;
-      }
-    });
-  }
-
-  
 function NeighbourhoodFilter() {
+    const [showFilter, setShowFilter] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(min-width: 1200px)')
 
-
+    if (mediaQuery.matches) {
+      setShowFilter(true);
+    }
+  }, []);
   return (
     <aside>
-      <nav>
+    <nav>
         <details open={showFilter}>
-          <summary>All Neighbourhoods</summary>
-          <ul className="neighbourhood-list">
-            {sortedNeighbourhoods.map((neighbourhood) => (
-              <li key={neighbourhood}>
-                <button>{neighbourhood.toUpperCase()}</button>
-              </li>
-            ))}
-          </ul>
+            <summary>
+                All Neighbourhoods
+            </summary>
+            <ul className="neighbourhood-list">
+              {sortedNeighbourhoods.map((aNeighbourhood) =>(
+                <li key={aNeighbourhood}><button>{aNeighbourhood.toUpperCase()}</button></li>
+              )) }
+
+            </ul>
         </details>
-      </nav>
-    </aside>
-  );
+    </nav>
+</aside>
+  )
 }
 
-export default NeighbourhoodFilter;
+export default NeighbourhoodFilter
